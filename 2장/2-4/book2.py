8126389d5d364690ce113dcfc49b385ad5046395
youngbin218 <50580794+youngbin218@users.noreply.github.com>
2021-02-18 22:55:24 +0900

    Create book2.py

---
+++ 2장/2-4/book2.py
@@ -0,0 +1,20 @@
+import scrapy
+
+class Book2Spider(scrapy.Spider):
+  name = 'book2'
+  start_urls = [
+    'https://wikibooks.co.kr/list/'
+  ]
+  
+  def parse(self, response):
+    li_list = response.css('.book-url')
+    for a in li_list:
+      href = a.css('::attr(href)').extract_first()
+      text = a.css('::text').extract_first()
+      
+      href2 = response.urljoin(href)
+      
+      yield {
+        'text' : text
+        'url' : href2
+      }
