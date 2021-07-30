fc7c74de2a883cec6e63d60c3e1e415d09a6b4c5
youngbin218 <youngbin218@naver.com>
2021-07-30 06:31:23 +0000

    Cross Validation 사용해보기(scikit-learn method 사용)

---
+++ 4장/4-7/cross-iris2.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from sklearn import svm, metrics, model_selection
+import random, re
+
+csv = pd.read_csv('iris.csv')
+
+data = csv[["sepal.length","sepal.width","petal.length","petal.width"]]
+label = csv["variety"]
+
+clf = svm.SVC()
+scores = model_selection.cross_val_score(clf, data, label, cv=5)
+print("각각의 정답률 = ", scores)
+print("평균 정답률 =", scores.mean())
