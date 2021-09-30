4d2b6d35897c8391ce7f22cb033fe392cc88abef
youngbin218 <youngbin218@naver.com>
2021-09-30 22:09:46 +0900

    머신러닝, 딥러닝 실전 개발 입문

--- ch7/7-5/ocr1.py
+++ ch7/7-5/ocr1.py
@@ -8,7 +8,10 @@ gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 blur = cv2.GaussianBlur(gray, (5,5), 0)
 thresh = cv2.adaptiveThreshold(blur, 255, 1, 1, 11, 2)
 
+# 윤곽 내의 윤곽을 추출할 때
 contours = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)[0]
+# 윤곽을 한 번만 추출할 때
+#contours = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
 
 for cnt in contours:
     x, y, w, h = cv2.boundingRect(cnt)
